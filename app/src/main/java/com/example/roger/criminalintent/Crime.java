539e6712ccf733227ee41ec82f269352b5ca9986
Roger-Han <roger.hanhong@Gmail.com>
2016-08-29 17:34:04 +1200

    Initial commit

---
+++ app/src/main/java/com/example/roger/criminalintent/Crime.java
@@ -0,0 +1,53 @@
+package com.example.roger.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Roger on 8/28/2016.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+}
